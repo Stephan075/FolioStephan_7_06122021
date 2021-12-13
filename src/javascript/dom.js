547f98f59cd5a0485d3f078399abf132ab455f28
Stephan <stephan-folio@hotmail.fr>
2021-12-13 21:28:07 +0100

    création et affichage du DOM sur le fichier JS

---
+++ src/javascript/dom.js
@@ -0,0 +1,86 @@
+// Un menu
+const card = (recipe) => {
+  const recipeDom = document.createElement('article')
+  recipeDom.className = 'recipe_card col-lg-4 mb-4'
+
+  let image = document.createElement('img')
+  image.src = './src/images/placeholder.jpg'
+  image.className = 'card-img-top'
+  image.setAttribute('alt', `image ${recipe.name}`)
+
+  recipeDom.appendChild(image)
+
+  let cardBody = document.createElement('div')
+  cardBody.className = 'card-body'
+
+  recipeDom.appendChild(cardBody)
+
+  let recipeInfo = document.createElement('div')
+  recipeInfo.className = 'recipe__info--first mb-2'
+
+  cardBody.appendChild(recipeInfo)
+
+  let cardTitle = document.createElement('h5')
+  cardTitle.className = 'card-title'
+
+  cardTitle.textContent = recipe.name
+
+  recipeInfo.appendChild(cardTitle)
+
+  let recipeIconAndTime = document.createElement('span')
+  recipeIconAndTime.className = 'recipe__iconAndTime'
+
+  let icon = document.createElement('i')
+  icon.className = 'far fa-clock'
+  recipeIconAndTime.appendChild(icon)
+  recipeIconAndTime.textContent = recipe.time + ' min'
+
+  recipeInfo.appendChild(recipeIconAndTime)
+
+  let recipeInfoSecond = document.createElement('div')
+  recipeInfoSecond.className = 'recipe__info--second'
+
+  cardBody.appendChild(recipeInfoSecond)
+
+  let divIngredients = document.createElement('div')
+  divIngredients.className = 'ingredients'
+
+  recipeInfoSecond.appendChild(divIngredients)
+
+  let ingredientList = document.createElement('ul')
+  ingredientList.className = 'list-inline ingredient__list'
+
+  divIngredients.appendChild(ingredientList)
+
+  // apl functon display ingredient
+  // recipe.ingredients.map(itemIngredient)
+  for (let ingredient of recipe.ingredients) {
+    // itemIngredient(ingredient)
+    ingredientList.appendChild(itemIngredient(ingredient))
+  }
+
+  let etape = document.createElement('p')
+  etape.className = 'etape'
+  etape.textContent = recipe.description
+
+  recipeInfoSecond.appendChild(etape)
+
+  document.querySelector('.recipes').appendChild(recipeDom)
+}
+
+const itemIngredient = (ingredient) => {
+  let ingredientList = document.createElement('li')
+  ingredientList.className = 'list-inline-item'
+
+  // unit => Optionel
+  ingredientList.textContent =
+    ingredient.ingredient +
+    ': ' +
+    (ingredient.quantite || ingredient.quantity || ' ') +
+    ' ' +
+    (ingredient.unit || ' ')
+
+  // console.log(ingredient)
+
+  return ingredientList
+}
--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,88 +1,55 @@
-const fetchRecipes = async () => {
-  try {
-    // récupérer la data
-    const url = './src/javascript/data/recipes.json'
-    const response = await fetch(url)
+// récupérer notre élement
+const searchInput = document.querySelector('#search')
+const recipesSection = document.querySelector('.recipes')
 
-    if (response.status < 300) {
-      return response.json()
-    }
-  } catch (e) {
-    console.log('erreur :', e)
-  }
-}
+// console.log(recipes)
 
 // ici on crée le DOM pour afficher les recettes
 const createRecipes = (recipes) => {
-  const recipeList = recipes
-  const recipeSection = document.querySelector('.recipes')
   // On récupére les recette 1/1
   const recipeElemDom = recipes.map((recipe) => {
-    console.log(recipe)
-    // Créer un article
-    const recipeDom = document.createElement('article')
-
-    recipeDom.classList.add('recipe_card', 'col-lg-4', 'mb-4')
-
-    // structure html
-    recipeDom.innerHTML = `
-<img src=" ./src/images/placeholder.jpg" class="card-img-top" alt="image ${recipe.name}">
-<div class="card-body">
-<div class="recipe__info--first mb-2">
-  <h5 class="card-title">${recipe.name}</h5>
-  <span class="recipe__iconAndTime"><i class="far fa-clock"></i>${recipe.time} min</span>
-</div>
-
-
-<div class="recipe__info--second">
-  <div class="ingredients">
-    <ul class="list-inline ingredient__list">
-      <li class="list-inline-item">
-        Lait de coco: 400ml
-      </li>
-      <li class="list-inline-item">
-        Jus de citron: 2
-      </li>
-      <li class="list-inline-item">
-        Créme de coco: 4
-      </li>
-      <li class="list-inline-item">
-        cuillères Sucre: 20g
-      </li>
-      <li class="list-inline-item">
-        Glaçons: 2
-      </li>
-    </ul>
-  </div>
-
-  <div class="etape">
-    Mettre les glaçons à votre goût dans le blender, ajouter le lait, la crème de coco, le jus de 2 citrons et
-    le
-    sucre. Mixer jusqu'à avoir la consistence désirée
-  </div>
-</div>
-
-</div>`
-    return recipeDom
+    card(recipe)
   })
-
-  // On remet le DOM à 0
-  recipeSection.innerHTML = ''
-
-  recipeSection.append(...recipeElemDom)
 }
 
 const filterRecipes = (recipes) => {
-  // récupérer notre élement
-  let input = document.querySelector('#search')
-  // const value = input.value
-
   // écouter l'évenement input
-  input.addEventListener('input', (e) => {
+
+  searchInput.addEventListener('keydown', (e) => {
     const value = e.target.value
+    // console.log(value);
 
     if (value.length > 2) {
-      console.log(recipes)
+      document.querySelector('.recipes').textContent = ''
+
+      createRecipes(
+        recipes.filter((recipe) => {
+          return recipe.name.toLowerCase().includes(value.toLowerCase())
+        })
+      )
+
+      createRecipes(
+        recipes.filter((recipe) => {
+          return recipe.ustensils.includes(value)
+        })
+      )
+
+      /*
+      createRecipes(
+        recipes.filter((recipe) => {
+          console.log(
+            recipe.ingredients.map((ingredient) => {
+              return ingredient.ingredient
+            })
+          )
+          return recipe.ingredients
+            .map((ingredient) => {
+              return ingredient.ingredient
+            })
+            .includes(value)
+        })
+      )
+      */
     }
   })
 }
@@ -91,12 +58,12 @@ const filterRecipes = (recipes) => {
 const initPage = async () => {
   try {
     // liste des recettes
-    const recipes = await fetchRecipes()
+    // const recipes = await fetchRecipes()
 
     // Créer le DOM
     createRecipes(recipes)
 
-    // filterRecipes(recipes)
+    filterRecipes(recipes)
   } catch (e) {
     console.log('erreur :', e)
   }
